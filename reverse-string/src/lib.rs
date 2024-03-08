c14aaea0f485596d7279f4d720bfb85fd35066f3
Christopher Leader <leaderc@oregonstate.edu>
2024-03-08 15:07:32 -0500

    Reverse String (Post-Solved)

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,6 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    todo!("Write a function to reverse {input}");
+    //input.chars().rev().collect::<String>() THIS CODE WORKS ON ONLY NON-GRAPHEME STRINGS.
+    input.graphemes(true).rev().collect::<String>()
 }
